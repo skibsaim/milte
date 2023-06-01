60c79d88e38936b0cf72b02d9271153b87704dd1
Sergio K. Quintana Gutierrez <skquintana@grupomavi.com>
2023-06-01 15:05:51 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: InicioComponent,
+
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
